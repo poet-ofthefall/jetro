1d46d39efda5d3675163ea4cb9ac236c461d1cce
poet-ofthefall <anastiasaenko@gmail.com>
2020-10-22 09:16:12 +0200

    remade the slider (as in the video), added better pictures

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
  $(document).ready(function(){
 
-   $('.slider__top').slick({
+   $('.slider').slick({
    slidesToShow: 1,
    fade: true,
-   
-   asNavFor: '.slider__bottom', 
+   asNavFor: '.thumbs', 
    // nextArrow: <button type="button" class="slick-next"><img src="images/slider/next.png" alt=""></button>,
    // prevArrow: <button class="slick-prev"><img src="images/slider/prev.png" alt=""></button>,  
  });
- $('.slider__bottom').slick({
+ $('.thumbs').slick({
    slidesToShow: 6,
+   slidesToScroll: 6,
    infinite: true,
-   asNavFor: '.slider__top',
+   asNavFor: '.slider',
    dots: false,
-   centerMode: true,
    focusOnSelect: true
  });
  });
